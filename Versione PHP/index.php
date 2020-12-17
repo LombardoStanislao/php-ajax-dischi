ce74baff2308eb30ec518b6e451bf4373feb81c5
Stanislao <stanislaoweb@libero.it>
2020-12-17 15:55:00 +0100

    Fatto lo stile molto semplice, problemi nell'importazione del file php

--- Versione PHP/index.php
+++ Versione PHP/index.php
@@ -1,10 +1,49 @@
+<?php
+
+  include _DIR_ .'dischi.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="../dist/app.css">
     <title>PHP | Dischi</title>
   </head>
   <body>
+    <header>
+      <h1>
+        PHP - JS - AJAX - DISCHI
+      </h1>
 
+    </header>
+
+    <main>
+
+      <div class="container">
+
+        <?php foreach ($dischi as $disco) { ?>
+
+          <div class="card">
+            <img src="<?php echo $disco['poster']; ?>" alt="">
+            <h4> New Jersey </h4>
+            <p>Bon Jovi</p>
+            <small> 1988 </small>
+
+
+          </div>
+
+        <?php
+        } ?>
+
+
+
+
+
+
+      </div>
+
+    </main>
   </body>
 </html>
